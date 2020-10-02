c9a865fa4c470f9cacffd8e1df307514bb3e8937
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2020-10-02 06:56:21 +0400

    SUM and COUNT

--- task4.sql
+++ task4.sql
@@ -11,7 +11,7 @@ SELECT name, CONCAT(ROUND(population/(SELECT population FROM world WHERE name = 
 -- 6
 SELECT name FROM world WHERE gdp > ALL(SELECT gdp from world WHERE continent = 'Europe' and gdp NOT LIKE 'null')
 -- 7
-SELECT continent, name, area FROM world xWHERE area >= ALL(SELECT area FROM world y WHERE y.continent=x.continent
+SELECT continent, name, area FROM world WHERE area >= ALL(SELECT area FROM world y WHERE y.continent=x.continent
 -- 8
 SELECT continent, name FROM world x WHERE name =(SELECT name FROM world y WHERE y.continent=x.continent ORDER BY name limit 1)
 -- 9
